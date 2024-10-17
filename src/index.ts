8941c1e85e3d85f0285e87e03fe4f917b6e7f876
mrinal-mann <mrinal.mann24@gmail.com>
2024-10-17 21:41:50 +0530

    chnage the post to false

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ server.listen(8080, function () {
 
 const wsServer = new WebSocketServer({
   httpServer: server,
-  autoAcceptConnections: true,
+  autoAcceptConnections: false,
 });
 
 function originIsAllowed(origin: string) {
